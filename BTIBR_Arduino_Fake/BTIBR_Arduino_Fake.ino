12854256438638eb2a9926019a46dc8f9d3ac91e
Rafael Falcaro <rafael.giarettafalcaro@gmail.com>
2021-11-29 22:16:21 -0300

    .

--- BTIBR_Arduino_Fake/BTIBR_Arduino_Fake.ino
+++ BTIBR_Arduino_Fake/BTIBR_Arduino_Fake.ino
@@ -8,24 +8,15 @@
 
 //***********************************************************************************************************************
 // Inclusão das Bibliotecas e Header dos pinos da placa
-#include <HX711.h>
 #include "modbus_rtu_slave.h"
 #include "PinoutConfig.h"
 
 
 //***********************************************************************************************************************
-// Criação dos Objetos
-HX711 StrainGauge;
-
-
-//***********************************************************************************************************************
 // Declaração de Variáveis
+int weightMeasure = 0;
 
-// Célula de Carga
-#define minWeight   0.010
-#define maxWeight   30.0
-#define scaleFactor -1110000.0f
-float weightMeasure = 0;
+// Geração dados Célula de Carga
 
 
 //***********************************************************************************************************************
@@ -33,14 +24,7 @@ float weightMeasure = 0;
 void setup() {
 
   // Comunicação Serial
-  Serial.begin(57600);
-
-  // Célula de Carga
-  StrainGauge.begin(pinDT, pinSCK);
-  StrainGauge.set_scale(scaleFactor);
-  delay(2000);
-  StrainGauge.tare();
-//  StrainGauge.power_up();
+  Serial.begin(9600);
 
   // Comunicação Modbus
   modbus_init(9600, RE_DE, SLAVE_ADDRS);
@@ -52,12 +36,10 @@ void setup() {
 //***********************************************************************************************************************
 // Rotina principal
 void loop() {
+  weightMeasure = random(95000, 105000);
 
-  weightMeasure = StrainGauge.get_units(5);
-
-  modbus_update_holding(0, weightMeasure);
+  modbus_update_holding(1, weightMeasure);
   modbus_check();
 
-  Serial.println(weightMeasure, 3);
-
+  Serial.println(weightMeasure);
 }
--- BTIBR_Arduino_Fake/PinoutConfig.h
+++ BTIBR_Arduino_Fake/PinoutConfig.h
@@ -1,10 +1,6 @@
 //********************************************************
 // Arquivo de configuração dos pinos da placa Arduino Nano
 
-// Célula de carga
-#define pinDT   2//A1
-#define pinSCK  3//A2
-
 // Comunicação Modbus
 #define RE_DE 2
 #define SLAVE_ADDRS 1
